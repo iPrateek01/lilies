1a50cbc17e0635a87f1d5100573f671298f9d5fa
Prateek Shende <prateek_655@live.com>
2024-10-03 01:51:33 +0530

    remove firebase.js in gitignore, install headlessui, add sample button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 // import React from 'react'
+import { Button } from "@headlessui/react"
 
 function Home() {
   return (
-    <div>Welcome Home</div>
+    <div>
+
+
+<Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
+      Save changes
+    </Button>
+
+
+    </div>
   )
 }
 
