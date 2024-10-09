85704c85ea3efdb8b39f3803c004dbc5c21e63fc
Prateek Shende <prateek_655@live.com>
2024-10-09 22:47:21 +0530

    adjusted the header padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <div className="navbar bg-customGreen te text-white">
+      <div className="navbar bg-customGreen te text-white p-5">
         <div className="flex-1">
           <a className="btn btn-ghost text-3xl">
             <img src="./assets/lilies.svg" alt="" className="size-9" />
@@ -18,13 +18,11 @@ function Header() {
             <Link to="/signup">
               <button
                 style={{ backgroundColor: "rgba(226, 184, 135, 1)" }}
-                className="btn text-black"
-              >
+                className="btn text-black">
                 Sign Up
               </button>
             </Link>
           </div>
-         
         </div>
       </div>
     </>
